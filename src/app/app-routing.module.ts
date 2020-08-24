a4bc67b5a8ff2fcf846cfa448d017fba80213096
0nk3 <0nk3.0nk3@gmail.com>
2020-08-24 15:05:58 +0200

    adding a new information page plus its route + back button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    path: "",
+    loadChildren: () => import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+  },
+  {
+    path: "activity-detail",
+    loadChildren: () => import("./activity-detail/activity-detail.module").then((m) => m.ActivityDetailPageModule),
+  },
 ];
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
